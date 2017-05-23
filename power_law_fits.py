3098e434537c5440383506dee6d7c7a51bd2c4a5
marcoviero <marco.viero@gmail.com>
2017-05-22 18:00:55 -0700

    tweaked

--- power_law_fits.py
+++ power_law_fits.py
@@ -12,34 +12,15 @@ import scipy.io
 from scipy import fftpack
 from lmfit import Parameters, minimize, fit_report
 from radial_data import radial_data
-pi=3.141592653589793
+from utils import clean_nans
+
+pi = 3.141592653589793
 L_sun = 3.839e26 # W
 c = 299792458.0 # m/s
 conv_sfr=1.728e-10/10 ** (.23)
 gc.enable()
 
-def simple_power_law_fitter(redshifts, lir, additional_features = {}, covar=None):
-    fit_params = Parameters()
-    fit_params.add('L0',value=9.5)
-    fit_params.add('gm_z',value= 1.8, vary = True, min = 0.001, max=2.5)
-    kws = {}
-    if covar != None: kws['covar']=covar
-    kws['lir'] = lir
-    j=1
-    for i in additional_features:
-        fit_params.add('gm_'+i, value= 1.0, vary = True,min=1e-5)
-        kws['feature'+str(j)] = additional_features[i]
-        j+=1
-
-    LMZpl_params = minimize(fit_simple_power_law,fit_params,
-        args = (np.ndarray.flatten(redshifts),),
-        kws  = kws)
-
-    m = LMZpl_params
-
-    return m
-
-def fit_simple_power_law(p, redshifts, lir, feature1=None, feature2=None, feature3=None,  feature4=None, covar = None):
+def fit_simple_power_law(p, redshifts, lir, feature1=None, feature2=None, feature3=None,  feature4=None, feature5=None, covar = None):
     v = p.valuesdict()
     A= np.asarray(v['L0'])
 
@@ -62,88 +43,120 @@ def fit_simple_power_law(p, redshifts, lir, feature1=None, feature2=None, featur
     else:
         return (np.log10(lir[ind]) - powerlaw[ind]) / np.log10(covar[ind])
 
-def build_power_laws(fn,exes):
-    pl = fn['M0']
-    weights = assign_weights(fn)
-    for key in weights:
-        pl += weights[key] * exes[key]
-    return pl
+def simple_power_law_fitter(redshifts, lir, additional_features = {}, covar=None):
+    fit_params = Parameters()
+    fit_params.add('L0',value=9.5)
+    fit_params.add('gm_z',value= 1.8, vary = True, min = 0.0001, max=3.0)
+    kws = {}
+    if covar != None: kws['covar']=covar
+    kws['lir'] = lir
+    j=1
+    for i in additional_features:
+        fit_params.add('gm_'+i, value= 1.0, vary = True,min=1e-5)
+        kws['feature'+str(j)] = additional_features[i]
+        j+=1
 
-def assign_weights(fn):
-    print fn.var_names
-    print 'L0=' + str(fn.params['L0'].value)
-    print 'gm_z=' + str(fn.params['gm_z'].value)
+    LMZpl_params = minimize(fit_simple_power_law,fit_params,
+        args = (np.ndarray.flatten(redshifts),),
+        kws  = kws)
+
+    m = LMZpl_params
+
+    return m
+
+def assign_weights(fn,silent=False):
+    if silent == True:
+        print fn.var_names
+        print 'L0=' + str(fn.params['L0'].value)
 
     weights = {}
 
-    weights['M0'] = fn.params['L0'].value
-    weights['gm_z'] = fn.params['gm_z'].value
+    weights['L0'] = fn.params['L0'].value
+    #weights['gm_z'] = fn.params['gm_z'].value
 
     try:
-        print 'gm_stellar_mass = ' +str(fn.params['gm_stellar_mass'].value)
-        weights['gm_mass'] = fn.params['gm_stellar_mass'].value
+        if silent == True:
+            print 'gm_z = ' +str(fn.params['gm_z'].value)
+        weights['gm_z'] = fn.params['gm_z'].value
+        weights['gm_z_z'] = 0.0
+    except:
+        weights['gm_z'] = 0.0
+        weights['gm_z_z'] = 0.0
+    try:
+        if silent == True:
+            print 'gm_mass = ' +str(fn.params['gm_mass'].value)
+        weights['gm_mass'] = fn.params['gm_mass'].value
         weights['gm_z_mass'] = 0.0
     except:
         weights['gm_mass'] = 0.0
         weights['gm_z_mass'] = 0.0
     try:
-        print 'gm_ahat = ' +str(fn.params['gm_a_hat'].value)
+        if silent == True:
+            print 'gm_ahat = ' +str(fn.params['gm_a_hat'].value)
         weights['gm_ahat'] = fn.params['gm_a_hat'].value
         weights['gm_z_ahat'] = 0.0
     except:
         weights['gm_ahat'] = 0.0
         weights['gm_z_ahat'] = 0.0
     try:
-        print 'gm_fratio = ' +str(fn.params['gm_f_ratio'].value)
+        if silent == True:
+            print 'gm_fratio = ' +str(fn.params['gm_f_ratio'].value)
         weights['gm_fratio'] = fn.params['gm_f_ratio'].value
         weights['gm_z_fratio'] = 0.0
     except:
         weights['gm_fratio'] = 0.0
         weights['gm_z_fratio'] = 0.0
     try:
-        print 'gm_uvj = ' + str(fn.params['gm_uvj'].value)
+        if silent == True:
+            print 'gm_uvj = ' + str(fn.params['gm_uvj'].value)
         weights['gm_uvj'] = fn.params['gm_uvj'].value
         weights['gm_z_uvj'] = 0.0
     except:
         weights['gm_uvj'] = 0.0
         weights['gm_z_uvj'] = 0.0
     try:
-        print 'gm_uv = ' + str(fn.params['gm_uv'].value)
+        if silent == True:
+            print 'gm_uv = ' + str(fn.params['gm_uv'].value)
         weights['gm_uv'] = fn.params['gm_uv'].value
         weights['gm_z_uv'] = 0.0
     except:
         weights['gm_uv'] = 0.0
         weights['gm_z_uv'] = 0.0
     try:
-        print 'gm_vj = ' + str(fn.params['gm_vj'].value)
+        if silent == True:
+            print 'gm_vj = ' + str(fn.params['gm_vj'].value)
         weights['gm_vj'] = fn.params['gm_uvj'].value
         weights['gm_z_vj'] = 0.0
     except:
         weights['gm_vj'] = 0.0
         weights['gm_z_vj'] = 0.0
     try:
-        print 'gm_a2t = ' + str(fn.params['gm_a2t'].value)
+        if silent == True:
+            print 'gm_a2t = ' + str(fn.params['gm_a2t'].value)
         weights['gm_a2t'] = fn.params['gm_a2t'].value
         weights['gm_z_a2t'] = 0.0
     except:
         weights['gm_a2t'] = 0.0
         weights['gm_z_a2t'] = 0.0
     try:
-        print 'gm_age = ' + str(fn.params['gm_age'].value)
+        if silent == True:
+            print 'gm_age = ' + str(fn.params['gm_age'].value)
         weights['gm_age'] = fn.params['gm_age'].value
         weights['gm_z_age'] = 0.0
     except:
         weights['gm_age'] = 0.0
         weights['gm_z_age'] = 0.0
     try:
-        print 'gm_tau = ' + str(fn.params['gm_tau'].value)
+        if silent == True:
+            print 'gm_tau = ' + str(fn.params['gm_tau'].value)
         weights['gm_tau'] = fn.params['gm_tau'].value
         weights['gm_z_tau'] = 0.0
     except:
         weights['gm_tau'] = 0.0
         weights['gm_z_tau'] = 0.0
     try:
-        print 'gm_Av = ' + str(fn.params['gm_Av'].value)
+        if silent == True:
+            print 'gm_Av = ' + str(fn.params['gm_Av'].value)
         weights['gm_Av'] = fn.params['gm_Av'].value
         weights['gm_z_Av'] = 0.0
     except:
@@ -151,3 +164,10 @@ def assign_weights(fn):
         weights['gm_z_Av'] = 0.0
 
     return weights
+
+def build_power_laws(fn,exes):
+    pl = fn.params['L0'].value
+    weights = assign_weights(fn)
+    for key in exes:
+        pl += weights['gm_'+key] * np.log10(exes[key])#[key]
+    return pl
