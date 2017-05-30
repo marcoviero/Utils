6aa4b85ca4aabb35c28d2aba274b6ad85b60c9f8
marcoviero <marco.viero@gmail.com>
2017-05-30 16:31:28 -0700

    debugged?

--- skymaps.py
+++ skymaps.py
@@ -395,14 +395,19 @@ class Field_catalogs:
 						if (self.table.rf_U_V.values[i] > (self.table.rf_V_J.values[i]*0.88+0.59) ): sfg[i]=0
 		self.table['sfg'] = sfg
 
-	def get_subpop_ids(self, znodes, mnodes, pop_dict, linear_mass=1, lookback_time = False):
+	def get_subpop_ids(self, znodes, mnodes, pop_dict, linear_mass=1, lookback_time = False, ndecimal=2):
 		self.subpop_ids = {}
+		decimal_pref = '{:.'+str(ndecimal)+'f}'
 		if lookback_time == True:
 			age_universe = cosmo.age(0).value # 13.797617455819209 Gyr
 			znodes = np.array([z_at_value(cosmo.age,(age_universe - i) * u.Gyr) for i in znodes])
 
 		for iz in range(len(znodes[:-1])):
+			zn = znodes[iz:iz+2]
+			z_suf = decimal_pref.format(zn[0])+'-'+decimal_pref.format(zn[1])
 			for jm in range(len(mnodes[:-1])):
+				mn = mnodes[jm:jm+2]
+				m_suf = decimal_pref.format(mn[0])+'-'+decimal_pref.format(mn[1])
 				for k in pop_dict:
 					if linear_mass == 1:
 						ind_mz =( (self.table.sfg.values == pop_dict[k][0]) & (self.table.z_peak.values >= np.min(znodes[iz:iz+2])) & (self.table.z_peak.values < np.max(znodes[iz:iz+2])) &
@@ -411,7 +416,13 @@ class Field_catalogs:
 						ind_mz =( (self.table.sfg == pop_dict[k][0]) & (self.table.z_peak >= np.min(znodes[iz:iz+2])) & (self.table.z_peak < np.max(znodes[iz:iz+2])) &
 							(self.table.LMASS >= np.min(mnodes[jm:jm+2])) & (self.table.LMASS < np.max(mnodes[jm:jm+2])) )
 
-					self.subpop_ids['z_'+clean_args(str(round(znodes[iz],3)))+'_'+clean_args(str(round(znodes[iz+1],3)))+'__m_'+clean_args(str(round(mnodes[jm],3)))+'_'+clean_args(str(round(mnodes[jm+1],3)))+'_'+k] = self.table.ID[ind_mz].values
+					#pdb.set_trace()
+					try:
+						arg = clean_args('z_'+z_suf+'__m_'+m_suf+'_'+pop_dict[k])
+					except:
+						arg = clean_args('z_'+z_suf+'__m_'+m_suf+'_'+k)
+					#self.subpop_ids['z_'+clean_args(str(decimal_pref(znodes[iz])))+'_'+clean_args(str(decimal_pref(znodes[iz+1])))+'__m_'+clean_args(str(decimal_pref(mnodes[jm])))+'_'+clean_args(str(decimal_pref(mnodes[jm+1])))+'_'+pop_dict[k]] = self.table.ID[ind_mz].values
+					self.subpop_ids[arg] = self.table.ID[ind_mz].values
 
 	def get_sf_qt_mass_redshift_bins(self, znodes, mnodes):
 		self.id_z_ms = {}
