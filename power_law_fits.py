86e7e98c00591b06e4f33e707905913916ce0623
marcoviero <marco.viero@gmail.com>
2017-05-30 19:13:57 -0700

    adding power_law_fits

--- power_law_fits.py
+++ power_law_fits.py
@@ -15,12 +15,79 @@ from radial_data import radial_data
 from utils import clean_nans
 from utils import clean_args
 from utils import completeness_flag_neural_net
+from utils import subset_averages_from_ids
 
 pi = 3.141592653589793
 L_sun = 3.839e26 # W
 c = 299792458.0 # m/s
 conv_sfr=1.728e-10/10 ** (.23)
 gc.enable()
+
+def fast_variable_power_law_wrapper(feature_dict_in, redshifts=None, Y_array=None, covar=None):
+    additional_features = {}
+    feature_dict = feature_dict_in.copy()
+    if redshifts == None:
+        redshifts = feature_dict.pop('z_peak')
+    if Y_array == None:
+        Y_array = feature_dict.pop('Y_val')
+    if covar == None:
+        covar = feature_dict.pop('Y_err')
+    for fkey in feature_dict:
+        additional_features[fkey] = {fkey: np.ndarray.flatten(np.array(feature_dict[fkey]))}
+    return fast_variable_power_law_fitter(np.ndarray.flatten(np.array(redshifts)), np.ndarray.flatten(np.array(Y_array)), additional_features = additional_features, covar=np.ndarray.flatten(np.array(covar)))
+
+def build_power_laws(fn,exes):
+    pl = fn.params['L0'].value
+    weights = assign_weights_cat_names(fn)
+    for key in exes:
+        pl += weights['gm_'+key] * np.log10(exes[key])#[key]
+    return pl
+
+def build_training_set(db,stacked_flux_densities, features_list, znodes, mnodes, knodes, Y_dict=None, k_init = 0, k_final = None, cc=0.5, ngal_cut=10):
+
+    nz = len(znodes)-1
+    nm = len(mnodes)-1
+    nk = len(knodes)
+    if k_final == None:
+        k_final = len(knodes)+1
+    training_set = {}
+    if Y_dict != None:
+        keys = [i for i in Y_dict.keys()]
+        key = keys[keys != 'Y_err']
+        training_set[key] = []
+        training_set['Y_err'] = []
+    for ft in features_list:
+        if ft in ['LMASS','lmass']:
+            training_set['stellar_mass'] = []
+        else:
+            training_set[ft] = []
+
+    for k in range(nk)[k_init:k_final]:
+        for im in range(nm):
+            mn = mnodes[im:im+2]
+            m_suf = '{:.2f}'.format(mn[0])+'-'+'{:.2f}'.format(mn[1])
+            for iz in range(nz):
+                zn = znodes[iz:iz+2]
+                z_suf = '{:.2f}'.format(zn[0])+'-'+'{:.2f}'.format(zn[1])
+
+                arg = clean_args('z_'+z_suf+'__m_'+m_suf+'_'+knodes[k])
+                ind = stacked_flux_densities.bin_ids[arg]
+                ngal = len(ind)
+                completeness_flag = completeness_flag_neural_net([np.mean(zn)],[np.mean(mn)],sfg=k, completeness_cut = cc)
+
+                if ((ngal > ngal_cut) & (completeness_flag == True)):
+                    if Y_dict != None:
+                        training_set[key].append(Y_dict[key][arg][0])
+                        training_set['Y_err'].append(Y_dict['Y_err'][arg])
+                    for ft in features_list:
+                        if ft in['ltau','lage','a_hat_AGN','la2t','Av']:
+                            training_set[ft].append(10**subset_averages_from_ids(db.table,ind,ft))
+                        elif ft in['LMASS','lmass']:
+                            training_set['stellar_mass'].append(10**subset_averages_from_ids(db.table,ind,ft))
+                        else:
+                            training_set[ft].append(subset_averages_from_ids(db.table,ind,ft))
+
+    return training_set
 
 def fit_simple_power_law(p, redshifts, lir, feature1=None, feature2=None, feature3=None,  feature4=None, feature5=None, covar = None):
     v = p.valuesdict()
@@ -125,8 +192,8 @@ def fast_variable_power_law_fitter(redshifts, lir, additional_features = {}, cov
     return m
 
 def assign_weights(fn,silent=True):
-    if silent == True:
-        print fn.var_names
+    if silent == False:
+        #print fn.var_names
         print 'L0=' + str(fn.params['L0'].value)
 
     weights = {}
@@ -135,7 +202,7 @@ def assign_weights(fn,silent=True):
     #weights['gm_z'] = fn.params['gm_z'].value
 
     try:
-        if silent == True:
+        if silent == False:
             print 'gm_z = ' +str(fn.params['gm_z'].value)
         weights['gm_z'] = fn.params['gm_z'].value
         try:
@@ -146,7 +213,7 @@ def assign_weights(fn,silent=True):
         weights['gm_z'] = 0.0
         weights['gm_z_z'] = 0.0
     try:
-        if silent == True:
+        if silent == False:
             print 'gm_mass = ' +str(fn.params['gm_mass'].value)
         weights['gm_mass'] = fn.params['gm_mass'].value
         try:
@@ -157,7 +224,7 @@ def assign_weights(fn,silent=True):
         weights['gm_mass'] = 0.0
         weights['gm_z_mass'] = 0.0
     try:
-        if silent == True:
+        if silent == False:
             print 'gm_ahat = ' +str(fn.params['gm_a_hat'].value)
         weights['gm_ahat'] = fn.params['gm_a_hat'].value
         try:
@@ -168,7 +235,7 @@ def assign_weights(fn,silent=True):
         weights['gm_ahat'] = 0.0
         weights['gm_z_ahat'] = 0.0
     try:
-        if silent == True:
+        if silent == False:
             print 'gm_fratio = ' +str(fn.params['gm_f_ratio'].value)
         weights['gm_fratio'] = fn.params['gm_f_ratio'].value
         try:
@@ -179,7 +246,7 @@ def assign_weights(fn,silent=True):
         weights['gm_fratio'] = 0.0
         weights['gm_z_fratio'] = 0.0
     try:
-        if silent == True:
+        if silent == False:
             print 'gm_uvj = ' + str(fn.params['gm_uvj'].value)
         weights['gm_uvj'] = fn.params['gm_uvj'].value
         try:
@@ -190,7 +257,7 @@ def assign_weights(fn,silent=True):
         weights['gm_uvj'] = 0.0
         weights['gm_z_uvj'] = 0.0
     try:
-        if silent == True:
+        if silent == False:
             print 'gm_uv = ' + str(fn.params['gm_uv'].value)
         weights['gm_uv'] = fn.params['gm_uv'].value
         try:
@@ -201,7 +268,7 @@ def assign_weights(fn,silent=True):
         weights['gm_uv'] = 0.0
         weights['gm_z_uv'] = 0.0
     try:
-        if silent == True:
+        if silent == False:
             print 'gm_vj = ' + str(fn.params['gm_vj'].value)
         weights['gm_vj'] = fn.params['gm_uvj'].value
         try:
@@ -212,7 +279,7 @@ def assign_weights(fn,silent=True):
         weights['gm_vj'] = 0.0
         weights['gm_z_vj'] = 0.0
     try:
-        if silent == True:
+        if silent == False:
             print 'gm_a2t = ' + str(fn.params['gm_a2t'].value)
         weights['gm_a2t'] = fn.params['gm_a2t'].value
         try:
@@ -223,7 +290,7 @@ def assign_weights(fn,silent=True):
         weights['gm_a2t'] = 0.0
         weights['gm_z_a2t'] = 0.0
     try:
-        if silent == True:
+        if silent == False:
             print 'gm_age = ' + str(fn.params['gm_age'].value)
         weights['gm_age'] = fn.params['gm_age'].value
         try:
@@ -234,7 +301,7 @@ def assign_weights(fn,silent=True):
         weights['gm_age'] = 0.0
         weights['gm_z_age'] = 0.0
     try:
-        if silent == True:
+        if silent == False:
             print 'gm_tau = ' + str(fn.params['gm_tau'].value)
         weights['gm_tau'] = fn.params['gm_tau'].value
         try:
@@ -245,7 +312,7 @@ def assign_weights(fn,silent=True):
         weights['gm_tau'] = 0.0
         weights['gm_z_tau'] = 0.0
     try:
-        if silent == True:
+        if silent == False:
             print 'gm_Av = ' + str(fn.params['gm_Av'].value)
         weights['gm_Av'] = fn.params['gm_Av'].value
         try:
@@ -258,52 +325,158 @@ def assign_weights(fn,silent=True):
 
     return weights
 
-def build_power_laws(fn,exes):
-    pl = fn.params['L0'].value
-    weights = assign_weights(fn)
-    for key in exes:
-        pl += weights['gm_'+key] * np.log10(exes[key])#[key]
-    return pl
+def assign_weights_cat_names(fn,silent=True):
+    if silent == False:
+        #print fn.var_names
+        print 'L0=' + str(fn.params['L0'].value)
 
-def build_training_set(stacked_flux_densities, features_list, znodes, mnodes, knodes, Y_dict=None, k_init = 0, k_final = None, cc=0.5, ngal_cut=10):
+    weights = {}
 
-    nz = len(znodes)-1
-    nm = len(mnodes)-1
-    nk = len(knodes)
-    if k_final == None:
-        k_final = len(knodes)+1
-    training_set = {}
-    if Y_dict != None:
-        keys = [i for i in Y_dict.keys()]
-        key = keys[keys != 'err']
-        training_set[key] = []
-        training_set['covar'] = []
-    for ft in features_list:
-        training_set[ft] = []
+    weights['L0'] = fn.params['L0'].value
+    #weights['gm_z'] = fn.params['gm_z'].value
 
-    for k in range(nk)[k_init:k_final]:
-        for im in range(nm):
-            mn = mnodes[im:im+2]
-            m_suf = '{:.2f}'.format(mn[0])+'-'+'{:.2f}'.format(mn[1])
-            for iz in range(nz):
-                zn = znodes[iz:iz+2]
-                z_suf = '{:.2f}'.format(zn[0])+'-'+'{:.2f}'.format(zn[1])
+    try:
+        if silent == False:
+            print 'gm_z = ' +str(fn.params['gm_z'].value)
+        weights['gm_z'] = fn.params['gm_z'].value
+        try:
+            if silent == False:
+                print 'gm_z_z = ' +str(fn.params['gm_z_z'].value)
+            weights['gm_z_z'] =  fn.params['gm_z_z'].value
+        except:
+            weights['gm_z_z'] = 0.0
+    except:
+        weights['gm_z'] = 0.0
+        weights['gm_z_z'] = 0.0
+    try:
+        if silent == False:
+            print 'gm_stellar_mass = ' +str(fn.params['gm_stellar_mass'].value)
+        weights['gm_stellar_mass'] = fn.params['gm_stellar_mass'].value
+        try:
+            if silent == False:
+                print 'gm_z_stellar_mass = ' +str(fn.params['gm_z_stellar_mass'].value)
+            weights['gm_z_stellar_mass'] =  fn.params['gm_z_stellar_mass'].value
+        except:
+            weights['gm_z_stellar_mass'] = 0.0
+    except:
+        weights['gm_stellar_mass'] = 0.0
+        weights['gm_z_stellar_mass'] = 0.0
+    try:
+        if silent == False:
+            print 'gm_ahat_AGN = ' +str(fn.params['gm_a_hat_AGN'].value)
+        weights['gm_ahat_AGN'] = fn.params['gm_a_hat_AGN'].value
+        try:
+            if silent == False:
+                print 'gm_z_a_hat_AGN = ' +str(fn.params['gm_z_a_hat_AGN'].value)
+            weights['gm_z_ahat_AGN'] =  fn.params['gm_z_ahat_AGN'].value
+        except:
+            weights['gm_z_ahat_AGN'] = 0.0
+    except:
+        weights['gm_ahat_AGN'] = 0.0
+        weights['gm_z_ahat_AGN'] = 0.0
+    try:
+        if silent == False:
+            print 'gm_F_ratio = ' +str(fn.params['gm_F_ratio'].value)
+        weights['gm_F_ratio'] = fn.params['gm_F_ratio'].value
+        try:
+            if silent == False:
+                print 'gm_z_F_ratio = ' +str(fn.params['gm_z_F_ratio'].value)
+            weights['gm_z_F_ratio'] =  fn.params['gm_z_F_ratio'].value
+        except:
+            weights['gm_z_F_ratio'] = 0.0
+    except:
+        weights['gm_F_ratio'] = 0.0
+        weights['gm_z_F_ratio'] = 0.0
+    try:
+        if silent == False:
+            print 'gm_UVJ = ' + str(fn.params['gm_UVJ'].value)
+        weights['gm_UVJ'] = fn.params['gm_UVJ'].value
+        try:
+            if silent == False:
+                print 'gm_z_UVJ = ' +str(fn.params['gm_z_UVJ'].value)
+            weights['gm_z_UVJ'] =  fn.params['gm_z_UVJ'].value
+        except:
+            weights['gm_z_UVJ'] = 0.0
+    except:
+        weights['gm_UVJ'] = 0.0
+        weights['gm_z_UVJ'] = 0.0
+    try:
+        if silent == False:
+            print 'gm_UV = ' + str(fn.params['gm_UV'].value)
+        weights['gm_UV'] = fn.params['gm_UV'].value
+        try:
+            if silent == False:
+                print 'gm_z_UV = ' +str(fn.params['gm_z_UV'].value)
+            weights['gm_z_UV'] =  fn.params['gm_z_UV'].value
+        except:
+            weights['gm_z_UV'] = 0.0
+    except:
+        weights['gm_UV'] = 0.0
+        weights['gm_z_UV'] = 0.0
+    try:
+        if silent == False:
+            print 'gm_VJ = ' + str(fn.params['gm_VJ'].value)
+        weights['gm_VJ'] = fn.params['gm_VJ'].value
+        try:
+            if silent == False:
+                print 'gm_z_VJ = ' +str(fn.params['gm_z_VJ'].value)
+            weights['gm_z_VJ'] =  fn.params['gm_z_VJ'].value
+        except:
+            weights['gm_z_VJ'] = 0.0
+    except:
+        weights['gm_VJ'] = 0.0
+        weights['gm_z_VJ'] = 0.0
+    try:
+        if silent == False:
+            print 'gm_la2t = ' + str(fn.params['gm_la2t'].value)
+        weights['gm_la2t'] = fn.params['gm_la2t'].value
+        try:
+            if silent == False:
+                print 'gm_z_la2t = ' +str(fn.params['gm_z_la2t'].value)
+            weights['gm_z_la2t'] =  fn.params['gm_z_la2t'].value
+        except:
+            weights['gm_z_la2t'] = 0.0
+    except:
+        weights['gm_la2t'] = 0.0
+        weights['gm_z_la2t'] = 0.0
+    try:
+        if silent == False:
+            print 'gm_lage = ' + str(fn.params['gm_lage'].value)
+        weights['gm_lage'] = fn.params['gm_lage'].value
+        try:
+            if silent == False:
+                print 'gm_z_lage = ' +str(fn.params['gm_z_lage'].value)
+            weights['gm_z_lage'] =  fn.params['gm_z_lage'].value
+        except:
+            weights['gm_z_lage'] = 0.0
+    except:
+        weights['gm_lage'] = 0.0
+        weights['gm_z_lage'] = 0.0
+    try:
+        if silent == False:
+            print 'gm_ltau = ' + str(fn.params['gm_ltau'].value)
+        weights['gm_ltau'] = fn.params['gm_ltau'].value
+        try:
+            if silent == False:
+                print 'gm_z_ltau = ' +str(fn.params['gm_z_ltau'].value)
+            weights['gm_z_ltau'] =  fn.params['gm_z_ltau'].value
+        except:
+            weights['gm_z_ltau'] = 0.0
+    except:
+        weights['gm_ltau'] = 0.0
+        weights['gm_z_ltau'] = 0.0
+    try:
+        if silent == False:
+            print 'gm_Av = ' + str(fn.params['gm_Av'].value)
+        weights['gm_Av'] = fn.params['gm_Av'].value
+        try:
+            if silent == False:
+                print 'gm_z_Av = ' +str(fn.params['gm_z_Av'].value)
+            weights['gm_z_Av'] =  fn.params['gm_z_Av'].value
+        except:
+            weights['gm_z_Av'] = 0.0
+    except:
+        weights['gm_Av'] = 0.0
+        weights['gm_z_Av'] = 0.0
 
-                arg = clean_args('z_'+z_suf+'__m_'+m_suf+'_'+knodes[k])
-                if Y_dict != None:
-                    training_set[key].append(Y_dict[key][arg][0])
-                    training_set['covar'].append(Y_dict['err'][arg])
-                ind = stacked_flux_densities.bin_ids[arg]
-                ngal = len(ind)
-                if ngal > ngal_cut:
-                    for ft in features_list:
-                        completeness_flag = completeness_flag_neural_net([np.mean(zn)],[np.mean(mn)],sfg=k, completeness_cut = cc)
-                        if completeness_flag == True:
-                            if ft in['LMASS','lmass','ltau','lage','a_hat_AGN','la2t','Av']:
-                                training_set[ft].append(10**subset_averages_from_ids(uVista.table,ind,ft))
-                            else:
-                                training_set[ft].append(subset_averages_from_ids(uVista.table,ind,ft))
-                        else:
-                            pass
-
-    return training_set
+    return weights
