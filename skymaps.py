1223af9ae9e7bda19ee7199a4a486fa4e4ee63c8
marcoviero <marco.viero@gmail.com>
2017-08-15 15:58:21 -0700

    passing self.map.copy to map_rms function

--- skymaps.py
+++ skymaps.py
@@ -77,7 +77,7 @@ class Skymaps:
 		self.header = hd
 		self.pixel_size = pix
 		self.psf = clean_nans(kern)
-		self.rms = map_rms(self.map, silent=False)
+		self.rms = map_rms(self.map.copy(), silent=True)
 
 		if wavelength != None:
 			add_wavelength(wavelength)
